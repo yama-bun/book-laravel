b9932ecf25098d60972b92b3aba5ca0dd524407c
yama-bun <baseball8296@gmail.com>
2021-02-05 06:04:26 +0900

    test

---
+++ app/Person.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    public function getData()
+    {
+        return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
+    }
+
+    public function scopeNameEqual($query, $str)
+    {
+        return $query->where('name', $str);
+    }
+
+    public function scopeAgeGreaterThan($query, $n)
+    {
+        return $query->where('age', '>=', $n);
+    }
+
+    public function scopeAgeLessThan($query, $n)
+    {
+        return $query->where('age', '<=', $n);
+    }
+}
